d2a8e0e54c453b89afaf15183ec941bc83124fe4
Barna <buzasbarnabas@gmail.com>
2021-05-26 11:21:51 +0200

    hiking almost final (need viridis, rename y label, replace legend)

--- A2_P1_data.R
+++ A2_P1_data.R
@@ -2,14 +2,18 @@ members <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/ti
 expeditions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/expeditions.csv')
 peaks <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-22/peaks.csv')
 
+install.packages("viridis")
+library(viridis)
+
 expeditions %>%
   mutate(new_peak_name = fct_lump(peak_name, n = 15)) %>%
   drop_na(new_peak_name) %>%
   filter(new_peak_name != "Other") %>%
-  ggplot(aes(x = fct_rev(fct_infreq(new_peak_name)), color = season)) +
+  ggplot(aes(x = fct_rev(fct_infreq(new_peak_name)), fill = season)) +
   coord_flip() +
-  geom_bar()
-
-
+  geom_bar() +
+  labs(title = "The 15 most popular peaks stacked by season of expedition") +
+  xlab(element_blank()) +
+  theme_light()
 
   
