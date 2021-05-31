76bee05aef403d33f962722a89efd4811e691efb
Barna <buzasbarnabas@gmail.com>
2021-05-31 14:34:51 +0200

    gecc

---
+++ A2_P3_re.R
@@ -0,0 +1,7 @@
+commute_mode <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-11-05/commute.csv")
+
+
+aggdata <- aggregate(n ~ state, commute_mode, sum)
+
+
+
