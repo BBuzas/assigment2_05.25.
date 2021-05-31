6b4994f960729743c036094e90a011360a9e5456
Barna <buzasbarnabas@gmail.com>
2021-05-31 10:25:19 +0200

    almost ready, missing: the name of the broad_field, the minimal style and the "Dark2" palette

--- A2_P2.R
+++ A2_P2.R
@@ -1,8 +1,10 @@
 phd_field <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-02-19/phd_by_field.csv")
 
-glimpse(phd_field)
-sum_broad_field <- phd_field %>%
-  summarize(broad_field)
-
-aggdata <- phd_field %>%
-  aggregate(by = sum_broad_field, 
+aggdata1 <- aggregate(n_phds ~ broad_field + year, phd_field, sum)
+aggdata1 %>%
+  ggplot(aes(x = year, y = n_phds, fill = broad_field, color = broad_field)) +
+  geom_line() +
+  scale_x_continuous(breaks = pretty_breaks(n = 5)) +
+  labs(title = "Number of awarded Ph.D.-s in the US by year",
+       x = element_blank(),
+       y = element_blank())
